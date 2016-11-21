4c5f7428fae3d10b25887c8928a454777cb179dd
Popescu Roxana <roxana.popescu@cegeka.com>
2016-11-21 17:11:27 +0200

    initial commit

---
+++ src/main/java/com/rentIT/RentItApplication.java
@@ -0,0 +1,12 @@
+package com.rentIT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentItApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RentItApplication.class, args);
+	}
+}
