4a3972af3e8e041c1d908ea3363c732ac9c5a7cd
Popescu Roxana <roxana.popescu@cegeka.com>
2016-12-06 17:23:12 +0200

    gulpfile

--- src/main/resources/static/gulpfile.js
+++ src/main/resources/static/gulpfile.js
@@ -2,7 +2,7 @@ var gulp = require('gulp');
 var inject = require('gulp-inject'),
     bowerFiles = require('main-bower-files'),
     angularFileSort = require('gulp-angular-filesort');
-+gulp.task('inject', function () {
+gulp.task('inject', function () {
     var target = gulp.src('index.html');
     var sources = gulp.src(bowerFiles(), {read: false});
 
