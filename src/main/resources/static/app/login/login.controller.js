f03aef7449dcd133ca9ab6ce83f64cfa72a752f3
rpopescu92 <popescu.roxana.andreea@gmail.com>
2016-12-14 14:42:54 +0200

    - register service frontend

--- src/main/resources/static/app/login/login.controller.js
+++ src/main/resources/static/app/login/login.controller.js
@@ -4,7 +4,22 @@
     angular.module('rentITApp')
             .controller('LoginController', LoginController);
 
-    function LoginController() {
+    LoginController.$inject = ['$scope', '$rootScope', '$mdDialog', '$state', 'AuthorizationService', 'Account', 'Principal'];
 
+    function LoginController($scope, $rootScope, $mdDialog, $state, AuthorizationService, Account, Principal) {
+
+        $scope.registerDialog = registerDialog;
+        $scope.currentUser = currentUser;
+
+        function registerDialog() {
+            $mdDialog.show({
+                templateUrl: 'app/register/register.html',
+                controller: 'RegisterController'
+            });
+        }
+
+        function currentUser() {
+
+        }
     }
 })();
---
+++ src/main/resources/static/app/register/register.controller.js
@@ -0,0 +1,13 @@
+(function() {
+    'use strict';
+
+     angular.module('rentITApp')
+            .controller('RegisterController', RegisterController);
+
+      RegisterController.$inject = ['$scope', 'RegisterService' '$mdDialog'];
+
+      function RegisterController($scope, RegisterService, $mdDialog) {
+
+      }
+
+})();
