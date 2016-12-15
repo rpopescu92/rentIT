c2ee0ad57ce775ca9c769c526553ba0c7de2ba84
rpopescu92 <popescu.roxana.andreea@gmail.com>
2016-12-15 13:59:35 +0200

    - register user

--- src/main/resources/static/app/register/register.controller.js
+++ src/main/resources/static/app/register/register.controller.js
@@ -7,7 +7,32 @@
       RegisterController.$inject = ['$scope', 'RegisterService', '$mdDialog'];
 
       function RegisterController($scope, RegisterService, $mdDialog) {
+           $scope.cancel = cancel;
+           $scope.register = register;
+           $scope.username = null;
+           $scope.errorMessage = null;
 
+
+           function cancel() {
+              $mdDialog.hide();
+           }
+
+           function register() {
+              var data = {
+                firstName: $scope.firstName,
+                lastName: $scope.lastName,
+                username: $scope.username,
+                password: $scope.password
+              }
+              RegisterService.register(data)
+                        .then(function(data) {
+                           $scope.errorMessage = "",
+                           $mdDialog.hide();
+                        },
+                        function(error) {
+                            $scope.errorMessage = error.data;
+                        });
+           }
       }
 
 })();
