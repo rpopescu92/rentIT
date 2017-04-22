52ab8a4e784677da88d68213fa05b7a7afdc1fb1
ciprian-constantin.hurmuzache <ciprian-constantin.hurmuzache@sky.uk>
2017-04-22 20:01:20 +0100

    - changes to my-properties and general changes

--- src/main/java/com/rentIT/RentItApplication.java
+++ src/main/java/com/rentIT/RentItApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class RentItApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(RentItApplication.class, args);
 	}
